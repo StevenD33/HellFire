88f9698ad61ebddd4e9dc03703bb6695b6d85549
Steven <steven.dias@ynov.com>
2019-05-14 11:36:47 +0200

    Modif menu
    add Audit.py en tant que fonction dans hellfire.py et modif du menu car librairie pas compatible pour mes besoins

--- Audit.py
+++ Audit.py
@@ -1,118 +1,122 @@
 #!/bin/python
-import os 
-import subprocess as sp
-def audit_debian():
-    print ("################################################")
-    print ("################################################")
-    print ("################################################")
-    print (" ('-. .-.   ('-.                                       _  .-')     ('-.   ")
-    print ("( OO )  / _(  OO)                                     ( \( -O )  _(  OO)  ")
-    print (",--. ,--.(,------.,--.      ,--.        ,------.,-.-') ,------. (,------. ")
-    print ("|  | |  | |  .---'|  |.-')  |  |.-') ('-| _.---'|  |OO)|   /`. ' |  .---' ")
-    print ("|   .|  | |  |    |  | OO ) |  | OO )(OO|(_\    |  |  \|  /  | | |  |     ")
-    print ("|       |(|  '--. |  |`-' | |  |`-' |/  |  '--. |  |(_/|  |_.' |(|  '--.  ")
-    print ("|  .-.  | |  .--'(|  '---.'(|  '---.'\_)|  .--',|  |_.'|  .  '.' |  .--'  ")
-    print ("|  | |  | |  `---.|      |  |      |   \|  |_)(_|  |   |  |\  \  |  `---. ")
-    print ("`--' `--' `------'`------'  `------'    `--'    `--'   `--' '--' `------' ")
-                                                            
-    print ("###############################################")
-    print ("Welcome to Hellfire:")
-    print ("###############################################")
-    print ("Script will automatically gather the required info:")
-    print ("The checklist can help you in the process of hardening your system:")
-    print ("Note: it has been tested for Debian Linux Distro:")
-    os.system('sleep 3')
-    print ("###############################################")
-    print ("OK....HOSTNAME..lets move on...wait for it to finish:")
-    os.system('sleep 3')
-    print ("Script Starts ;)")
-    os.system('START=$(date +%s)') # add fonction pour timer 
-    print  (" Linux Kernel Information")
-    os.system('uname -a')
-    print ("###############################################")
-    print  (" Current User and ID information")
-    os.system('whoami')
-    os.system('id')
-    print ("###############################################")
-    print ("Linux Distribution Information")
-    os.system('lsb_release -a')
-    print ("###############################################")
-    print  ("List Current Logged In Users")
-    os.system('w')
-    print ("###############################################")
-    print ("HOSTNAME uptime Information")
-    os.system('uptime')
-    print ("###############################################")
-    print  ("Running Services")
-    os.system('service --status-all |grep "+"')
-    print ("###############################################")
-    print  ("Active internet connections and open ports")
-    os.system('netstat -natp')
-    print ("###############################################")
-    print  ("Check Available Space")
-    os.system('df')
-    print ("###############################################")
-    print  ("Check Memory")
-    os.system('free -h')
-    print ("###############################################")
-    print ("Check SSH Configuration")
-    os.system('cat /etc/ssh/sshd_config')
-    print ("###############################################")
-    print ("###############################################")
-    print ("Network Parameters")
-    os.system('cat /etc/sysctl.conf')
-    print ("###############################################")
-    print ("Password Policies")
-    os.system('cat /etc/pam.d/common-password')
-    print ("###############################################")
-    print (" Check your Source List file")
-    os.system('cat /etc/apt/sources.list')
-    print ("###############################################")
-    print  ("Check for broken dependencies")
-    os.system('apt-get check')
-    print ("###############################################")
-    print  ("MOTD banner message")
-    os.system('cat /etc/motd')
-    print ("###############################################")
-    print ("List user names")
-    os.system('cut -d: -f1 /etc/passwd')
-    print ("###############################################")
-    print ("###############################################")
-    #print("Socket Investigation")
-    #os.system('ss')
-    #print ("###############################################")
-    print ("#--------------------------------------------------------------------------------------------------------------")
-    print ("# SSH Setup")
-    print ("#--------------------------------------------------------------------------------------------------------------")
-    port_ssh = sp.getoutput('grep -E "Port " /etc/ssh/sshd_config')
-
-    if port_ssh == "#Port 22":
-        print("SHD Config: Port is set to default (22).  Recommend change to a non-standard port to make your SSH server more difficult to find/notice.  (Remember to restart SSHD with /etc/init.d/ssh restart after making changes)")
-    else:
-        print(port_ssh)
-
-    ssh_listen_address = sp.getoutput('grep -E "ListenAddress ::" /etc/ssh/sshd_config')
-    if ssh_listen_address == "#ListenAddress ::":
-        print("SSHD Config: ListenAddress is set to default (all addresses).  SSH will listen on ALL available IP addresses.  Recommend change to a single IP to reduce the number of access points.  (Remember to restart SSHD with /etc/init.d/ssh restart after making changes)")
-    else:
-        print(ssh_listen_address)
-        
-    print('END')
+def audit():
+    import os 
+    import subprocess as sp
 
 
+    def audit_debian():
+        print ("################################################")
+        print ("################################################")
+        print ("################################################")
+        print (" ('-. .-.   ('-.                                       _  .-')     ('-.   ")
+        print ("( OO )  / _(  OO)                                     ( \( -O )  _(  OO)  ")
+        print (",--. ,--.(,------.,--.      ,--.        ,------.,-.-') ,------. (,------. ")
+        print ("|  | |  | |  .---'|  |.-')  |  |.-') ('-| _.---'|  |OO)|   /`. ' |  .---' ")
+        print ("|   .|  | |  |    |  | OO ) |  | OO )(OO|(_\    |  |  \|  /  | | |  |     ")
+        print ("|       |(|  '--. |  |`-' | |  |`-' |/  |  '--. |  |(_/|  |_.' |(|  '--.  ")
+        print ("|  .-.  | |  .--'(|  '---.'(|  '---.'\_)|  .--',|  |_.'|  .  '.' |  .--'  ")
+        print ("|  | |  | |  `---.|      |  |      |   \|  |_)(_|  |   |  |\  \  |  `---. ")
+        print ("`--' `--' `------'`------'  `------'    `--'    `--'   `--' '--' `------' ")
+                                                                
+        print ("###############################################")
+        print ("Welcome to Hellfire:")
+        print ("###############################################")
+        print ("Script will automatically gather the required info:")
+        print ("The checklist can help you in the process of hardening your system:")
+        print ("Note: it has been tested for Debian Linux Distro:")
+        os.system('sleep 3')
+        print ("###############################################")
+        print ("OK....HOSTNAME..lets move on...wait for it to finish:")
+        os.system('sleep 3')
+        print ("Script Starts ;)")
+        os.system('START=$(date +%s)') # add fonction pour timer 
+        print  (" Linux Kernel Information")
+        os.system('uname -a')
+        print ("###############################################")
+        print  (" Current User and ID information")
+        os.system('whoami')
+        os.system('id')
+        print ("###############################################")
+        print ("Linux Distribution Information")
+        os.system('lsb_release -a')
+        print ("###############################################")
+        print  ("List Current Logged In Users")
+        os.system('w')
+        print ("###############################################")
+        print ("HOSTNAME uptime Information")
+        os.system('uptime')
+        print ("###############################################")
+        print  ("Running Services")
+        os.system('service --status-all |grep "+"')
+        print ("###############################################")
+        print  ("Active internet connections and open ports")
+        os.system('netstat -natp')
+        print ("###############################################")
+        print  ("Check Available Space")
+        os.system('df')
+        print ("###############################################")
+        print  ("Check Memory")
+        os.system('free -h')
+        print ("###############################################")
+        print ("Check SSH Configuration")
+        os.system('cat /etc/ssh/sshd_config')
+        print ("###############################################")
+        print ("###############################################")
+        print ("Network Parameters")
+        os.system('cat /etc/sysctl.conf')
+        print ("###############################################")
+        print ("Password Policies")
+        os.system('cat /etc/pam.d/common-password')
+        print ("###############################################")
+        print (" Check your Source List file")
+        os.system('cat /etc/apt/sources.list')
+        print ("###############################################")
+        print  ("Check for broken dependencies")
+        os.system('apt-get check')
+        print ("###############################################")
+        print  ("MOTD banner message")
+        os.system('cat /etc/motd')
+        print ("###############################################")
+        print ("List user names")
+        os.system('cut -d: -f1 /etc/passwd')
+        print ("###############################################")
+        print ("###############################################")
+        #print("Socket Investigation")
+        #os.system('ss')
+        #print ("###############################################")
+        print ("#--------------------------------------------------------------------------------------------------------------")
+        print ("# SSH Setup")
+        print ("#--------------------------------------------------------------------------------------------------------------")
+        port_ssh = sp.getoutput('grep -E "Port " /etc/ssh/sshd_config')
 
+        if port_ssh == "#Port 22":
+            print("SHD Config: Port is set to default (22).  Recommend change to a non-standard port to make your SSH server more difficult to find/notice.  (Remember to restart SSHD with /etc/init.d/ssh restart after making changes)")
+        else:
+            print(port_ssh)
 
-Distrib = os.uname()
-
-print (Distrib[2])
+        ssh_listen_address = sp.getoutput('grep -E "ListenAddress ::" /etc/ssh/sshd_config')
+        if ssh_listen_address == "#ListenAddress ::":
+            print("SSHD Config: ListenAddress is set to default (all addresses).  SSH will listen on ALL available IP addresses.  Recommend change to a single IP to reduce the number of access points.")
+        else:
+            print(ssh_listen_address)
+            
+        print('END')
 
 
 
-if  'MANJARO' in Distrib[2] :
-   audit_debian()
-if 'kali' in Distrib[2]:
-    audit_debian()
-if 'debian' in Distrib[2] :
-    audit_debian()
-#if 'ubuntu' in Distrib[2] :
-#    audit_ubuntu()
+
+    Distrib = os.uname()
+
+    print (Distrib[2])
+
+
+
+    if  'MANJARO' in Distrib[2] :
+        audit_debian()
+    if 'kali' in Distrib[2]:
+        audit_debian()
+    if 'debian' in Distrib[2] :
+        audit_debian()
+    #if 'ubuntu' in Distrib[2] :
+    #    audit_ubuntu()
+
--- HellFire.py
+++ HellFire.py
@@ -1,34 +1,124 @@
 # Import the necessary packages
 from consolemenu import *
 from consolemenu.items import *
-
-
+from Audit import audit
 # Create the menu
-menu = ConsoleMenu("Hellfire")
+# menu = ConsoleMenu("Hellfire")
 
-# Create some items
-# A CommandItem runs a console command
+# # Create some items
+# # A CommandItem runs a console command
 
-audit_item = CommandItem("Run Audit on your system",  "python Audit.py", should_exit=True)
-basic_hardening_item = CommandItem("Run Basic Hardening on your system",  "python Basic_Hardening.py")
-advanced_hardening_item = CommandItem("Run Advanced Hardening on your system",  "python Advanced_Hardening.py")
-personalised_hardening_item = CommandItem("Run Personalised Hardening on your system",  "python Personalised_Hardening.py")
+# audit_item = CommandItem("Run Audit on your system",  "python Audit.py",)
+# basic_hardening_item = CommandItem("Run Basic Hardening on your system",  "python Basic_Hardening.py")
+# advanced_hardening_item = CommandItem("Run Advanced Hardening on your system",  "python Advanced_Hardening.py")
+# personalised_hardening_item = CommandItem("Run Personalised Hardening on your system",  "python Personalised_Hardening.py")
+
+# function_item = FunctionItem("Call a Python function",audit)
+
+# # A SelectionMenu constructs a menu from a list of strings
+# selection_menu = SelectionMenu(["item1", "item2", "item3"])
+
+# # A SubmenuItem lets you add a menu (the selection_menu above, for example)
+# # as a submenu of another menu
+# submenu_item = SubmenuItem("Submenu item", selection_menu, menu)
+
+# # Once we're done creating them, we just add the items to the menu
+# menu.append_item(audit_item)
+# menu.append_item(basic_hardening_item)
+# menu.append_item(advanced_hardening_item)
+# menu.append_item(personalised_hardening_item)
+# menu.append_item(submenu_item)
+# menu.append_item(function_item)
+
+# menu.show()
 
 
+import sys, os
+
+# Main definition - constants
+menu_actions  = {}  
+
+# Main menu
+def main_menu():
+    os.system('clear')
+    print (" ('-. .-.   ('-.                                       _  .-')     ('-.   ")
+    print ("( OO )  / _(  OO)                                     ( \( -O )  _(  OO)  ")
+    print (",--. ,--.(,------.,--.      ,--.        ,------.,-.-') ,------. (,------. ")
+    print ("|  | |  | |  .---'|  |.-')  |  |.-') ('-| _.---'|  |OO)|   /`. ' |  .---' ")
+    print ("|   .|  | |  |    |  | OO ) |  | OO )(OO|(_\    |  |  \|  /  | | |  |     ")
+    print ("|       |(|  '--. |  |`-' | |  |`-' |/  |  '--. |  |(_/|  |_.' |(|  '--.  ")
+    print ("|  .-.  | |  .--'(|  '---.'(|  '---.'\_)|  .--',|  |_.'|  .  '.' |  .--'  ")
+    print ("|  | |  | |  `---.|      |  |      |   \|  |_)(_|  |   |  |\  \  |  `---. ")
+    print ("`--' `--' `------'`------'  `------'    `--'    `--'   `--' '--' `------' ")
+    print ("Welcome,\n")
+    print ("1. Run Audit on your system")
+    print ("2. Run Basic Hardening on your system")
+    print ("3. Run Advanced Hardening on your system")
+    print ("4. Run Personalised Hardening on your system")
+    print ("\n0. Quit")
+    choice = input(" >>  ")
+    exec_menu(choice)
+
+    return
+
+# Execute menu
+def exec_menu(choice):
+    os.system('clear')
+    ch = choice.lower()
+    if ch == '':
+        menu_actions['main_menu']()
+    else:
+        try:
+            menu_actions[ch]()
+        except KeyError:
+            print ("Invalid selection, please try again.\n")
+            menu_actions['main_menu']()
+    return
+
+# Menu 1
+def menu1():
+    audit()
 
 
-# A SelectionMenu constructs a menu from a list of strings
-selection_menu = SelectionMenu(["item1", "item2", "item3"])
+# Menu 2
+def menu2():
+    print ("Hello Menu 2 !\n")
+    choice = input(" >>  ")
+    exec_menu(choice)
+    return
 
-# A SubmenuItem lets you add a menu (the selection_menu above, for example)
-# as a submenu of another menu
-submenu_item = SubmenuItem("Submenu item", selection_menu, menu)
+def menu3():
+    print ("Hello Menu 2 !\n")
+    choice = input(" >>  ")
+    exec_menu(choice)
+    return
+def menu4():
+    print ("Hello Menu 2 !\n")
+    choice = input(" >>  ")
+    exec_menu(choice)
+    return
 
-# Once we're done creating them, we just add the items to the menu
-menu.append_item(audit_item)
-menu.append_item(basic_hardening_item)
-menu.append_item(advanced_hardening_item)
-menu.append_item(personalised_hardening_item)
-menu.append_item(submenu_item)
 
-menu.show()
+# Back to main menu
+def back():
+    menu_actions['main_menu']()
+
+# Exit program
+def exit():
+    sys.exit()
+
+
+# Menu definition
+menu_actions = {
+    'main_menu': main_menu,
+    '1': menu1,
+    '2': menu2,
+    '9': back,
+    '0': exit,
+}
+
+
+# Main Program
+if __name__ == "__main__":
+    # Launch main menu
+    main_menu()
