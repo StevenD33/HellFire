5b9c0c8917156acea629a8b8a63f508c594857e6
Steven-Kali <steven.dias@ynov.com>
2019-05-15 12:20:02 -0400

    correction test.py ready for implementation in Audit.py

--- test.py
+++ test.py
@@ -2,19 +2,19 @@ import os
 import subprocess as sp
 
 
-x11_forwarding = sp.getoutput('grep -E X11Forwarding /etc/ssh/sshd_config')
+x11_forwarding = sp.getoutput('grep -E X11Forwarding /etc/ssh/sshd_config | head -1')
 
 
 if x11_forwarding != "X11Forwarding no"  :
-    print("SSHD Config: X11Forwarding should be set to no (unless needed).")
+    print("SSHD Config: X11Forwarding should be set to no (unless needed).\n")
 else:
-    print(X11Forwarding)
+    print(x11_forwarding)
 
 strict_modes = sp.getoutput('grep -E StrictModes /etc/ssh/sshd_config')
 
 
 if strict_modes != "StrictModes yes"  :
-    print("SSHD Config: StrictModes should be set to yes (to check file permissions of files such as ~/.ssh, ~/.ssh/authorized_keys etc).")
+    print("SSHD Config: StrictModes should be set to yes (to check file permissions of files such as ~/.ssh, ~/.ssh/authorized_keys etc).\n")
 else:
     print(strict_modes)
 
@@ -24,15 +24,15 @@ ignore_rhosts = sp.getoutput('grep -E IgnoreRhosts /etc/ssh/sshd_config')
 
 
 if ignore_rhosts != "IgnoreRhosts yes"  :
-    print("SSHD Config: IgnoreRhosts should be set to yes (this method of Authentication should be avoided).")
+    print("SSHD Config: IgnoreRhosts should be set to yes (this method of Authentication should be avoided).\n")
 else:
     print(ignore_rhosts)
 
-host_base_authentication = sp.getoutput('grep -E HostbasedAuthentication /etc/ssh/sshd_config')
+host_base_authentication = sp.getoutput('grep -E HostbasedAuthentication /etc/ssh/sshd_config | head -1')
 
 
 if host_base_authentication != "HostbasedAuthentication no"  :
-    print("SSHD Config: HostbasedAuthentication should be set to no (this method of Authentication should be avoided).")
+    print("SSHD Config: HostbasedAuthentication should be set to no (this method of Authentication should be avoided).\n")
 else:
     print(host_base_authentication)
 
@@ -40,14 +40,13 @@ rhost_rsa_authentication = sp.getoutput('grep -E RhostsRSAAuthentication /etc/ss
 
 
 if rhost_rsa_authentication != "RhostsRSAAuthentication no"  :
-    print("SSHD Config: RhostsRSAAuthentication should be set to no (this method of Authentication should be avoided).")
+    print("SSHD Config: RhostsRSAAuthentication should be set to no (this method of Authentication should be avoided).\n")
 else:
     print(rhost_rsa_authentication)
 
 gateway_ports = sp.getoutput('grep -E GatewayPorts /etc/ssh/sshd_config')
-
-if gateway_ports != ""  :
-    print("SSHD Config: GatewayPorts is configured.  These allow listening on non-localhost addresses on the server.  This is disabled by default, but has been added to the config file.  Recommend remove this setting unless needed.")
+if gateway_ports != "GatewayPorts no" :
+    print("SSHD Config: GatewayPorts is configured.  These allow listening on non-localhost addresses on the server.  This is disabled by default, but has been added to the config file.  Recommend remove this setting unless needed.\n")
 else:
     print(gateway_ports)
 
