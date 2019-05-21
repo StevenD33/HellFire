b2b2c03a43e0c193167d3ff71fc5ac5bfb8cd3f4
Steven <steven.dias@ynov.com>
2019-05-21 11:40:21 +0200

    correction bu audit.py

--- test.py
+++ test.py
@@ -1,24 +1,27 @@
-import subprocess
+import subprocess as sp
 import os.path
 
 
 
 
-subprocess.call("passwd -l root".split())
-print("root has been locked!")
+#subprocess.call("passwd -l root".split())
+#print("root has been locked!")
 
 #/etc/security/access.conf -:root: ALL EXCEPT LOCAL
-if os.path.exists("/etc/security/access.conf") == True:
-    with open("/etc/security/access.conf", "a") as myfile:
-        myfile.write("\n-:root: ALL EXCEPT LOCAL")
-    print("access.conf edited!")
-else:
-    print("/etc/security/access.conf not found!")
+#if os.path.exists("/etc/security/access.conf") == True:
+#    with open("/etc/security/access.conf", "a") as myfile:
+ #       myfile.write("\n-:root: ALL EXCEPT LOCAL")
+#    print("access.conf edited!")
+#else:
+#    print("/etc/security/access.conf not found!")
 
-if os.path.exists("/etc/lightdm/lightdm.conf") == True:
-    with open("/etc/lightdm/lightdm.conf", "a") as myfile:
-        myfile.write("\nallow-guest=false")
+#if os.path.exists("/etc/lightdm/lightdm.conf") == True:
+#    with open("/etc/lightdm/lightdm.conf", "a") as myfile:
+ #       myfile.write("\nallow-guest=false")
+#
+ #   print("guest locked!")
+#else:
+   # print("/etc/lightdm/lightdm.conf does not exist")
 
-    print("guest locked!")
-else:
-    print("/etc/lightdm/lightdm.conf does not exist")
+os.system("chmod +x ./bash_script/password.sh")
+sp.call("./bash_script/password.sh")
