f9a24e0b9a2d723fcb22d04dd2d7065dae4f0bc1
Steven <steven.dias@ynov.com>
2019-05-21 11:23:38 +0200

    menu fonctionnel

--- HellFire.py
+++ HellFire.py
@@ -58,51 +58,11 @@ def main_menu():
     print ("4. Run Personalised Hardening on your system")
     print ("\n0. Quit")
     choice = input(" >>  ")
-    exec_menu(choice)
+    if choice == "1":
+        audit()
+    elif choice == "2":
+            basic_hardening()
 
-    return
-
-# Execute menu
-def exec_menu(choice):
-    os.system('clear')
-    ch = choice()
-    if ch == '':
-        menu_actions['main_menu']()
-    else:
-        try:
-            menu_actions[ch]()
-        except KeyError:
-            print ("Invalid selection, please try again.\n")
-            menu_actions['main_menu']()
-    return
-
-# Menu 1
-def menu1():
-    audit()
-
-
-# Menu 2
-def menu2():
-    basic_hardening()
-
-
-# Back to main menu
-def back():
-    menu_actions['main_menu']()
-
-# Exit program
-def exit():
-    sys.exit()
-
-
-# Menu definition
-menu_actions = {
-    'main_menu': main_menu,
-    '1': menu1,
-    '2': menu2,
-    '9': back,
-    '0': exit,
-}
 
 
 # Main Program
